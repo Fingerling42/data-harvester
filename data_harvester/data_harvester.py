55e61cedad36dbd16895a201e013914c4382f1c7
Fingerling42 <bermanivan42@gmail.com>
2024-01-17 21:22:20 +0200

    Added subscribers to all odom sensors and ApproximateTimeSynchronizer for recording their readings

--- data_harvester/data_harvester.py
+++ data_harvester/data_harvester.py
@@ -11,6 +11,8 @@ import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
 
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+
 from ament_index_python.packages import get_package_share_directory
 
 # Groups that cannot being executed in parallel to avoid deadlocks
@@ -19,7 +21,8 @@ from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
 
 from irobot_create_msgs.action import WallFollow, Undock
-from irobot_create_msgs.msg import DockStatus, Mouse
+from irobot_create_msgs.msg import DockStatus, Mouse, IrIntensityVector
+from sensor_msgs.msg import Imu
 
 from slam_toolbox.srv import SaveMap, SerializePoseGraph
 
@@ -111,13 +114,42 @@ class DataHarvester(Node):
             self.timer_workload_callback,
         )
 
-        # Creating subscriber to mouse sensor
-        self.subscriber_mouse = self.create_subscription(
+        # Creating subscribers to odom sensors
+        self.subscriber_mouse = Subscriber(
+            self,
             Mouse,
             'mouse',
-            self.subscriber_mouse_callback,
-            qos_profile_sensor_data,
+            qos_profile=qos_profile_sensor_data,
         )
+
+        self.subscriber_imu = Subscriber(
+            self,
+            Imu,
+            'imu',
+            qos_profile=qos_profile_sensor_data,
+        )
+
+        self.subscriber_cliff = Subscriber(
+            self,
+            IrIntensityVector,
+            'cliff_intensity',
+            qos_profile=qos_profile_sensor_data,
+        )
+
+        self.subscriber_ir_bumper = Subscriber(
+            self,
+            IrIntensityVector,
+            'ir_intensity',
+            qos_profile=qos_profile_sensor_data,
+        )
+
+        queue_size = 30
+        self.odom_topics_synchronizer = ApproximateTimeSynchronizer(
+            [self.subscriber_mouse, self.subscriber_imu, self.subscriber_cliff, self.subscriber_ir_bumper],
+            queue_size,
+            0.01,
+        )
+        self.odom_topics_synchronizer.registerCallback(self.record_odom)
 
     def timer_workload_callback(self):
         """
@@ -128,7 +160,7 @@ class DataHarvester(Node):
 
         # Check if the robot is docked, if not - start undocking
         if self.dock_status is True:
-            self.get_logger().info("The robot is docked, need to undock it first")
+            self.get_logger().info('The robot is docked, need to undock it first')
             self.send_goal_undock()
             self.undock_done_event.wait()
 
@@ -144,22 +176,87 @@ class DataHarvester(Node):
         self.serialize_map()
         self.map_serializer_done_event.wait()
 
+        self.get_logger().info('Saving odometry to JSON file in workspace dir...')
         self.odom_file.close()
 
-        self.get_logger().info("All done")
+        self.get_logger().info('All done')
 
-    def subscriber_mouse_callback(self, msg):
+    def record_odom(self, mouse_msg, imu_msg, cliff_msg, bumper_ir_msg):
         if self.wall_follow_done_event.is_set() is not True:
-            timestamp = float(msg.header.stamp.sec + msg.header.stamp.nanosec * pow(10, -9))
-            integrated_x = float(msg.integrated_x)
-            integrated_y = float(msg.integrated_y)
+            timestamp = float(mouse_msg.header.stamp.sec + mouse_msg.header.stamp.nanosec * pow(10, -9))
 
-            mouse_dict = {'timestamp': timestamp,
-                          'integrated_x': integrated_x,
-                          'integrated_y': integrated_y,
-                          }
+            # Mouse sensor
+            mouse_integrated_x = float(mouse_msg.integrated_x)
+            mouse_integrated_y = float(mouse_msg.integrated_y)
 
-            json.dump(mouse_dict, self.odom_file, indent=4)
+            # IMU
+            imu_orientation_x = float(imu_msg.orientation.x)
+            imu_orientation_y = float(imu_msg.orientation.y)
+            imu_orientation_z = float(imu_msg.orientation.z)
+            imu_orientation_w = float(imu_msg.orientation.w)
+            imu_ang_vel_x = float(imu_msg.angular_velocity.x)
+            imu_ang_vel_y = float(imu_msg.angular_velocity.y)
+            imu_ang_vel_z = float(imu_msg.angular_velocity.z)
+            imu_linear_acc_x = float(imu_msg.linear_acceleration.x)
+            imu_linear_acc_y = float(imu_msg.linear_acceleration.y)
+            imu_linear_acc_z = float(imu_msg.linear_acceleration.z)
+
+            # Cliff IR sensors
+            cliff_side_left_intensity = int(cliff_msg.readings[0].value)
+            cliff_front_left_intensity = int(cliff_msg.readings[1].value)
+            cliff_front_right_intensity = int(cliff_msg.readings[2].value)
+            cliff_side_right_intensity = int(cliff_msg.readings[3].value)
+
+            # IR sensors on front bumper
+            bumper_side_left_intensity = int(bumper_ir_msg.readings[0].value)
+            bumper_left_intensity = int(bumper_ir_msg.readings[1].value)
+            bumper_front_left_intensity = int(bumper_ir_msg.readings[2].value)
+            bumper_front_center_left_intensity = int(bumper_ir_msg.readings[3].value)
+            bumper_front_center_right_intensity = int(bumper_ir_msg.readings[4].value)
+            bumper_front_right_intensity = int(bumper_ir_msg.readings[5].value)
+            bumper_right_intensity = int(bumper_ir_msg.readings[6].value)
+
+            sensor_dict = {'timestamp': timestamp,
+                           'mouse_sensor': {
+                               'integrated_x': mouse_integrated_x,
+                               'integrated_y': mouse_integrated_y,
+                           },
+                           'imu': {
+                               'orientation': {
+                                   'x': imu_orientation_x,
+                                   'y': imu_orientation_y,
+                                   'z': imu_orientation_z,
+                                   'w': imu_orientation_w,
+                               },
+                               'angular_velocity': {
+                                   'x': imu_ang_vel_x,
+                                   'y': imu_ang_vel_y,
+                                   'z': imu_ang_vel_z,
+                               },
+                               'linear_acceleration': {
+                                   'x': imu_linear_acc_x,
+                                   'y': imu_linear_acc_y,
+                                   'z': imu_linear_acc_z,
+                               }
+                           },
+                           'cliff_ir': {
+                               'cliff_side_left': cliff_side_left_intensity,
+                               'cliff_front_left': cliff_front_left_intensity,
+                               'cliff_front_right': cliff_front_right_intensity,
+                               'cliff_side_right': cliff_side_right_intensity,
+                           },
+                           'bumper_ir': {
+                               'bumper_side_left': bumper_side_left_intensity,
+                               'bumper_left': bumper_left_intensity,
+                               'bumper_front_left': bumper_front_left_intensity,
+                               'bumper_front_center_left': bumper_front_center_left_intensity,
+                               'bumper_front_center_right': bumper_front_center_right_intensity,
+                               'bumper_front_right': bumper_front_right_intensity,
+                               'bumper_right': bumper_right_intensity,
+                           }
+                           }
+
+            json.dump(sensor_dict, self.odom_file, indent=4)
 
     def save_map(self):
         """
