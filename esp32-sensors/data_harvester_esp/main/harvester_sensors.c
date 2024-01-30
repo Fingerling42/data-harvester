dc26468c8dc750256732c497f0978e1006a810c9
Fingerling42 <bermanivan42@gmail.com>
2024-01-30 17:14:31 +0200

    Added Wi-Fi connection to ESP32

--- esp32-sensors/data_harvester_esp/main/harvester_sensors.c
+++ esp32-sensors/data_harvester_esp/main/harvester_sensors.c
@@ -5,7 +5,14 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
 
 // Include header files of sensors
 #include "sht3x.h"
@@ -15,10 +22,27 @@
 // Tag of executable for logging
 static const char *TAG = "harvester_sensors";
 
+// FreeRTOS event group to signal when wi-fi is connected
+static EventGroupHandle_t s_wifi_event_group;
+static int s_retry_num = 0;
+
 // Devices descriptors
 static sht3x_t sht3x_dev;
 static i2c_dev_t bh1750_dev;
-// static i2c_dev_t scd4x_dev = { 0 };
+static i2c_dev_t scd4x_dev = { 0 };
+
+// Bits for event group: connected to the AP with an IP or failed to connect after the maximum amount of retries
+#define WIFI_CONNECTED_BIT 			BIT0
+#define WIFI_FAIL_BIT      			BIT1
+
+// Wi-Fi connection parameters
+#define WIFI_SSID      						""
+#define WIFI_PASS      						""
+#define MAXIMUM_RETRY  						5
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD	WIFI_AUTH_WPA2_PSK
+#define ESP_WIFI_SAE_MODE					WPA3_SAE_PWE_BOTH
+#define EXAMPLE_H2E_IDENTIFIER				""
+
 
 // I2C pin numbers
 #define I2C_MASTER_SCL_IO           19     // GPIO number used for I2C master clock
@@ -29,11 +53,101 @@ static i2c_dev_t bh1750_dev;
 #define SHT3X_ADDR					0x44
 #define BH1750_ADDR					BH1750_ADDR_LO
 
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "Retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"Connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "Got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            .threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
+            .sae_pwe_h2e = ESP_WIFI_SAE_MODE,
+            .sae_h2e_identifier = EXAMPLE_H2E_IDENTIFIER,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "Wi-Fi initialization is finished.");
+
+    // Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+    // number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler()
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "Connected to AP SSID: %s", WIFI_SSID);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID: %s", WIFI_SSID);
+    } else {
+        ESP_LOGE(TAG, "Unexpected event");
+    }
+}
+
 
 void app_main(void)
 {
 	struct timeval tv_now;
 	float timestamp;
+
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+	// Connect to Wi-Fi access point
+	wifi_init();
 
 	// Initialize I2C
 	ESP_ERROR_CHECK(i2cdev_init());
@@ -59,7 +173,6 @@ void app_main(void)
     // Initialize SCD4x sensor
     uint16_t co2;
     float temp_scd4x, humi_scd4x;
-    i2c_dev_t scd4x_dev = { 0 };
 
     scd4x_init_desc(&scd4x_dev, I2C_MASTER_BUS, I2C_MASTER_SDA_IO, I2C_MASTER_SCL_IO);
     scd4x_wake_up(&scd4x_dev);
