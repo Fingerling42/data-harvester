f06c16bbf1f2b14cd5a99bba26f5667a500f0346
Fingerling42 <bermanivan42@gmail.com>
2024-02-02 17:16:23 +0200

    Added ROS2 publisher of sensors readings

--- data_harvester_sensors/data_harvester_sensors/esp32_sensors_node.py
+++ data_harvester_sensors/data_harvester_sensors/esp32_sensors_node.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.qos import qos_profile_sensor_data
 
 from data_harvester_interfaces.msg import DataHarvesterESPSensors
 
@@ -40,6 +41,14 @@ class ESP32SensorNode(Node):
             callback_group=sensors_callback_group,
         )
 
+        # Publisher of sensors readings
+        self.publisher_sensors_readings = self.create_publisher(
+            DataHarvesterESPSensors,
+            'data_harvester/esp_sensors',
+            qos_profile=qos_profile_sensor_data,
+            callback_group=sensors_callback_group,
+        )
+
     def timer_sensors_callback(self):
         """
         Timer callback after which sensors readings starts
@@ -54,6 +63,26 @@ class ESP32SensorNode(Node):
             self.esp_serial.port = self.esp_port.value
             self.esp_serial.open()
             self.get_logger().info('Connected to ESP device on given port')
+
+            # Start main loop
+            while True:
+                # Read one line from serial
+                line = self.esp_serial.readline().decode('utf-8')
+                # Check if line is consist sensors readings in JSON format
+                if line.startswith('{"timestamp"'):
+                    readings_json = json.loads(line)
+
+                    esp_sensors_msg = DataHarvesterESPSensors()
+                    esp_sensors_msg.header.stamp = self.get_clock().now().to_msg()
+                    esp_sensors_msg.temperature = float('%.2f' % readings_json['temperature'])
+                    esp_sensors_msg.humidity = float('%.2f' % readings_json['humidity'])
+                    esp_sensors_msg.luminosity = int(readings_json['luminosity'])
+                    esp_sensors_msg.co2 = int(readings_json['co2'])
+
+                    self.publisher_sensors_readings.publish(esp_sensors_msg)
+                else:
+                    continue
+
         except serial.serialutil.SerialException:
             self.get_logger().error('Serial port not found')
             self.executor.remove_node(self)
@@ -88,7 +117,7 @@ def main(args=None):
         except KeyboardInterrupt:
             # Close serial connection and kill node
             esp32_sensors_node.esp_serial.close()
-            esp32_sensors_node.get_logger().warn("Killing the ESP32 sensors node...")
+            esp32_sensors_node.get_logger().warn("Closing the port and killing the ESP32 sensors node...")
             executor.remove_node(esp32_sensors_node)
 
 
