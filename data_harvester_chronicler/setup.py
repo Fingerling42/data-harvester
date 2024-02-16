c3a98dbdea5a2806a96ad29d800736ef523e19d4
Fingerling42 <bermanivan42@gmail.com>
2024-02-16 15:15:44 +0200

    Created separate pkg for recording all robots data

---
+++ data_harvester_chronicler/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'data_harvester_chronicler'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Ivan Berman',
+    maintainer_email='berman@robonomics.network',
+    description='The package for recording all harvested data',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
